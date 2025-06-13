b6a17e2feeef9111beb7e61308924a3c54398590
ali <applealijafaripour@gmail.com>
2025-06-13 11:29:14 +0330

    cart desgining

--- Desktop/Coffee-shop/src/app/app.js
+++ Desktop/Coffee-shop/src/app/app.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const themeToggle = document.querySelector("#toggle-theme");
+
+themeToggle.addEventListener("click", () => {
+  if (localStorage.theme == "dark") {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+const themeChange = () => {
+  if (localStorage.theme == "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+};
